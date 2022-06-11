168ca791d843016988e7859f7495591664d974be
Евгений Терехов <nergal@psu.ru>
2022-06-11 15:46:11 +0500

    added Approval tests library to project

---
+++ src/test/java/kata/PackageSettings.java
@@ -0,0 +1,8 @@
+package kata;
+
+/**
+ * Class to be injected in Approval-tests. It contains the settings for all the tests inside given package.
+ */
+public class PackageSettings {
+    public static String ApprovalBaseDirectory = "../resources/approvals";
+}
